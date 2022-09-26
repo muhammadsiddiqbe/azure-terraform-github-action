86faae94a4301ed524f27ba58163d12ab56c30cb
Muhammadsiddiq <muhammadsiddiqbe@gmail.com>
2022-09-26 10:54:07 +0500

    add queue container blob storage

--- StorageAccounts/main.tf
+++ StorageAccounts/main.tf
@@ -26,3 +26,38 @@ resource "azurerm_storage_account" "primary" {
     default_action = "Allow"
   }
 }
+
+resource "azurerm_storage_account" "secondary" {
+  name                     = "ssecondary${random_string.random.result}"
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+
+  network_rules {
+    bypass         = ["AzureServices", "Logging", "Metrics"]
+    default_action = "Allow"
+  }
+}
+
+resource "azurerm_storage_queue" "storage_queue" {
+  name                 = "${random_string.random.result}queue"
+  storage_account_name = azurerm_storage_account.primary.name
+}
+
+resource "azurerm_storage_container" "storage_container" {
+  name                  = "${random_string.random.result}vhds"
+  storage_account_name  = azurerm_storage_account.primary.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "storage_blob" {
+  name = "herpderp1.vhd"
+
+  storage_account_name   = azurerm_storage_account.primary.name
+  storage_container_name = azurerm_storage_container.storage_container.name
+
+  type = "Page"
+  size = 5120
+}
