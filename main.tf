8c2d80e8587fc101a7109bc01d65299290b8dce4
Muhammadsiddiq <muhammadsiddiqbe@gmail.com>
2022-09-26 09:08:10 +0500

    add vnet and update

--- main.tf
+++ main.tf
@@ -26,17 +26,31 @@ output "object_id" {
 
 ######
 
-module "ResourceGroup" {
-  source = "./ResourceGroup"
+module "ResourceGroups" {
+  source = "./ResourceGroups"
 
   resource_group_name = "MainRG"
   resource_group_location = "eastus"
   environment = var.environment
 }
 
+module "VirtualNetworks" {
+  source = "./VirtualNetworks"
+
+  public_ip_primary_name = "${var.project}_primary_ip"
+  environment = var.environment
+  virtual_network_primary_name = "${var.project}_primary_vn"
+  location = var.location
+  subnet_primary_name = "${var.project}_primary_subnet"
+  resource_group_name = module.ResourceGroups.rg_name_out
+  network_interface_primary_nic_name = "${var.project}_primary_nic"
+  my_terraform_nsg = "${var.project}_primary_nsg"
+
+}
+
 module "StorageAccount" {
-  source = "./StorageAccount"
+  source = "./StorageAccounts"
   base_name = "mypro"
-  resource_group_name = module.ResourceGroup.rg_name_out
+  resource_group_name = module.ResourceGroups.rg_name_out
   location = var.location
 }
