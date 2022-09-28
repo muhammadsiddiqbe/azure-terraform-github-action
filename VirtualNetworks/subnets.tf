115a388fb43a3e907df440f2f5c497723e66ef0b
Muhammadsiddiq <muhammadsiddiqbe@gmail.com>
2022-09-28 14:14:26 +0500

    fix subnet issue

--- VirtualNetworks/subnets.tf
+++ VirtualNetworks/subnets.tf
@@ -16,7 +16,7 @@ resource "azurerm_subnet" "subnet-2" {
   name                 = "sn-con-nest-${var.environment}-${var.location}-01"
   resource_group_name  = var.resource_group_name
   virtual_network_name = var.virtual_network_primary_name
-  address_prefixes     = var.subnet_primary_address_prefixes
+  address_prefixes     = var.subnet_secondary_address_prefixes
 
   enforce_private_link_endpoint_network_policies = true
 
